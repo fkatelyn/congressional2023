2fd706f39fa4fbec2d7f734f9df62c9c17f9331b
fkatelyn <fkatelyn@gmail.com>
2023-09-24 18:55:56 -0700

    Add PalmVision with SwiftUI framework

---
+++ PalmVision/PalmVision/PalmVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalmVisionApp.swift
+//  PalmVision
+//
+//  Created by Katelyn Fritz on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct PalmVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
