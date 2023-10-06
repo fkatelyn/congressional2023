b316f120ffadacfafa7f3cb2b883150b19395a70
fkatelyn <fkatelyn@gmail.com>
2023-10-05 23:50:51 -0700

    Add version 0 wireframe
    - Figma version 0 wireframe

--- PalmVision/PalmVision/ContentView.swift
+++ PalmVision/PalmVision/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Image("palmtree")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
