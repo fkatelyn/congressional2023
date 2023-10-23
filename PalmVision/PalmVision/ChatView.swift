6c7017da90dc376663c525e31b54e1ae753711b0
Katelyn Fritz <fkatelyn@gmail.com>
2023-10-22 22:05:29 -0700

    XCode internal files removal
    - Add Video and Chat view

---
+++ PalmVision/PalmVision/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  PalmVision
+//
+//  Created by Katelyn Fritz on 10/22/23.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
---
+++ PalmVision/PalmVision/VideoView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoView.swift
+//  PalmVision
+//
+//  Created by Katelyn Fritz on 10/22/23.
+//
+
+import SwiftUI
+
+struct VideoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoView()
+}
